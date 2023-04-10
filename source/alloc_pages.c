4f45e0fa8baf43254906d9e746c5b3ac294666ac
linuxzjs <2453726928@qq.com>
2023-04-10 22:26:06 +0800

    调整代码结构

---
+++ source/alloc_pages.c
@@ -0,0 +1,22 @@
+#include <linux/hrtimer.h>
+#include <linux/kernel.h>
+#include <linux/kallsyms.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/timex.h>
+#include <linux/tracepoint.h>
+#include <trace/events/irq.h>
+#include <linux/proc_fs.h>
+#include <linux/init.h>
+#include <linux/sysctl.h>
+#include <trace/events/napi.h>
+#include <linux/rtc.h>
+#include <linux/time.h>
+#include <linux/rbtree.h>
+#include <linux/list.h>
+#include <linux/version.h>
+#include <linux/sort.h>
+#include <linux/vmalloc.h>
+
+
